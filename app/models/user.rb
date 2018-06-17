f3faacb354ae1eaeb69d6943485ac1bcfa21ca0c
Yuki Haita <hatsuisa0921@gmail.com>
2018-06-17 22:06:48 +0900

    password requires minimum 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :name, uniqueness: true
   validates :name, presence: true
   validates :karui, presence: true
-
+  validates :password, length: {minimum:6}
   has_secure_password
 end
