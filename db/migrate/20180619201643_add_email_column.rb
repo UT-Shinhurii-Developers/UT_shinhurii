962f7c909cbf55576e9eda9cd4412bc2e230d9bd
Yuki Haita <hatsuisa0921@gmail.com>
2018-06-20 12:54:24 +0900

    Change

--- db/migrate/20180619201643_add_email_column.rb
+++ db/migrate/20180619201643_add_email_column.rb
@@ -1,5 +1,5 @@
 class AddEmailColumn < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :email, :string, null: false, default: "", unique: true
+    #add_column :users, :email, :string, null: false, default: "", unique: true
   end
 end
