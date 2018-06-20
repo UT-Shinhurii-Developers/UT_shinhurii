d0f4b948535cebc7a1404c27ac8df53849e8e8a2
Yuki Haita <hatsuisa0921@gmail.com>
2018-06-20 12:48:58 +0900

    Change

--- db/migrate/20180619142152_add_columns_to_users.rb
+++ db/migrate/20180619142152_add_columns_to_users.rb
@@ -1,6 +1,5 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :email, :string
     add_column :users, :year, :integer
     add_column :users, :dep1, :string
     add_column :users, :dep2, :string
